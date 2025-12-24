ad93ec8df8448c98c9671ad9e88c8bcc8e1451e7
KolomarenkoDmytrii <kolobok575lg@gmail.com>
2025-12-24 16:50:39 +0200

    Add prototype for file uploading

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@
 # https://docs.trychroma.com/integrations/embedding-models/ollama
 
 import os
+import pathlib
 
-from fastapi import FastAPI
+# from fastapi import FastAPI, File, UploadFile
+import fastapi
 
 import chromadb
 from chromadb.utils.embedding_functions import HuggingFaceEmbeddingFunction
@@ -15,6 +17,7 @@ from chromadb.utils.embedding_functions.ollama_embedding_function import (
 # from langchain_ollama import OllamaEmbeddings, OllamaLLM
 
 LLM_MODEL = "embeddinggemma"
+DATA_DIRECTORY = pathlib.Path("data")
 # Adjust the base URL as per your Ollama server configuration
 OLLAMA_URL = "http://ollama:11434"
 
@@ -31,7 +34,8 @@ def load_txt_files(directory):
                 txt_files.append(file.read())
     return (txt_files, names)
 
-app = FastAPI()
+
+app = fastapi.FastAPI()
 
 chroma_client = chromadb.PersistentClient()
 collection = chroma_client.get_or_create_collection(
@@ -41,9 +45,9 @@ collection = chroma_client.get_or_create_collection(
         url=OLLAMA_URL,
     ),
 )
-docs, ids = load_txt_files("./data/docs")
+# docs, ids = load_txt_files("./data/docs")
 
-collection.add(documents=docs, ids=ids)
+# collection.add(documents=docs, ids=ids)
 
 # done = False
 # while not done:
@@ -59,6 +63,36 @@ collection.add(documents=docs, ids=ids)
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/search")
-def read_item(query: str):
-    return collection.query(query_texts=[query], n_results=3)
+def search_docs(query: str):
+    return collection.query(query_texts=[query], n_results=30)
+
+
+@app.post("/upload")
+def upload_doc(file: fastapi.UploadFile):
+    if not file.content_type == "text/plain":
+        raise fastapi.HTTPException(status_code=400, detail="Not supported file type")
+
+    contents = file.file.read()
+
+    try:
+        with open(DATA_DIRECTORY / "docs" / file.filename, "wb") as f:
+            f.write(contents)
+
+    except Exception:
+        raise fastapi.HTTPException(
+            status_code=500, detail="Error occured during saving the uploaded file"
+        )
+    finally:
+        file.file.close()
+
+    try:
+        collection.add(documents=[contents.decode("utf-8")], ids=[file.filename]) # TODO: винести в окрему функцію
+    except Exception:
+        raise fastapi.HTTPException(
+            status_code=500, detail="Error occured during processing the uploaded file"
+        )
+    # collection.add(documents=[contents.decode("utf-8")], ids=[file.filename])
+    
+    return {"message": f"File {file.filename} was successfuly uploaded"}
