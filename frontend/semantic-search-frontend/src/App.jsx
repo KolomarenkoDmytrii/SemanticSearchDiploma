d5e5f6f9e022f7cbde4301c09cf64bee3b79b8bd
KolomarenkoDmytrii <kolobok575lg@gmail.com>
2026-01-03 20:48:30 +0200

    Added file download functionality to the frontend

--- frontend/semantic-search-frontend/src/App.jsx
+++ frontend/semantic-search-frontend/src/App.jsx
@@ -1,15 +1,43 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { BrowserRouter, Route, Routes, Link, useNavigate } from 'react-router-dom';
 
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
+import Overlay from 'react-bootstrap/Overlay';
+import Badge from 'react-bootstrap/Badge';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Stack from 'react-bootstrap/Stack';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+
+// import 'bootstrap/dist/css/bootstrap.css';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
 
 const API_URL = "http://localhost:8000";
 
 
+function FileDownloadLink({ filename }) {
+  return <a href={API_URL + '/files/download/' + filename}>{filename}</a>;
+}
+
+function SearchResultsItem({ filename, text }) {
+  return <Card>
+    <Card.Body>
+      <Card.Title><FileDownloadLink filename={filename} /></Card.Title>
+      <Card.Text>{text}</Card.Text>
+    </Card.Body>
+  </Card>
+}
+
 function SearchResults({ results }) {
-  if (!results?.ids) return <p>...</p>
+  if (!results?.ids) return <></>
 
   const results_number = results.ids[0].length;
   let items = [];
@@ -17,43 +45,55 @@ function SearchResults({ results }) {
     items.push({ id: results.ids[0][i], filename: results.metadatas[0][i]['filename'], text: results.documents[0][i] });
   }
 
-  return <ul>
-    {items.map(({ id, filename, text }) => <li key={id}><b>{filename}</b><br /><p>{text}</p></li>)}
-  </ul>
+  // return <ul>
+  //   {items.map(({ id, filename, text }) =>
+  //     <li key={id}><b><FileDownloadLink filename={filename} /></b><br /><p>{text}</p></li>)}
+  // </ul>
+  return <ListGroup>
+    {items.map(({ id, filename, text }) =>
+      <ListGroup.Item key={id}><SearchResultsItem filename={filename} text={text} /></ListGroup.Item>)}
+  </ListGroup>
 }
 
-function SearchForm() {
+function SearchForm({ setResults }) {
   const [query, setQuery] = useState('');
-  const [results, setResults] = useState({});
+  // const [results, setResults] = useState({});
   const [isSearchError, setIsSearchError] = useState(false);
 
   function handleSearch(e) {
     e.preventDefault()
 
-    fetch(API_URL + "/search" + `?query=${query}`)
+    fetch(API_URL + '/search' + `?query=${query}`)
       .then((response) => response.json())
       .then((json) => { setResults(json); setIsSearchError(false); })
       .catch((error) => setIsSearchError(true))
   }
 
-  return <>
-    <div>
-      <form onSubmit={handleSearch}>
-        <label>Query:
-          <input type="text" value={query} onChange={e => setQuery(e.target.value)} />
-        </label>
-        <button type="submit">Search</button>
-      </form>
-      {isSearchError ? (<p>Error occured during search</p>) : (<SearchResults results={results} />)}
-    </div>
-  </>
+  return <Form onSubmit={handleSearch}>
+    <Row>
+      <Col>
+        <Form.Control type="text" placeholder="Enter search query..." value={query} onChange={e => setQuery(e.target.value)} />
+      </Col>
+      <Col>
+        <Button variant="primary" type="submit">Search</Button>
+      </Col>
+    </Row>
+    {isSearchError && (<Row><p>Error occured during search</p></Row>)}
+  </Form>
 }
 
 function Home() {
+  const [results, setResults] = useState({});
+
   return <>
-    <h1>Searcher</h1>
-    <SearchForm />
-  </>
+    {/* <div className="fixed-top" style={{ position: 'absolute', paddingTop: '20px + var(--bs-navbar-height)' }}> */}
+    <div>
+      <SearchForm setResults={setResults} />
+    </div>
+    <div>
+      <SearchResults results={results} />
+    </div>
+  </>;
 }
 
 function FileUploadForm({ updateFilesListHandler }) {
@@ -64,8 +104,8 @@ function FileUploadForm({ updateFilesListHandler }) {
     formData.append('file', selectedFile);
 
     try {
-      await fetch(API_URL + "/files/upload/", {
-        method: "POST",
+      await fetch(API_URL + '/files/upload/', {
+        method: 'POST',
         body: formData,
       });
 
@@ -76,36 +116,59 @@ function FileUploadForm({ updateFilesListHandler }) {
     }
   }
 
-  return <div>
-    <h3>Upload a file to the server</h3>
-    <p>
-      Selected file: <input type="file" onChange={(event) => setSelectedFile(event.target.files[0])} />
-      {selectedFile && <button onClick={onFileUpload}>Upload!</button>}
-    </p>
-  </div>;
+  return <Form.Group>
+    <Form.Label>Upload a file to the server</Form.Label>
+    <Stack direction="horizontal" gap={3}>
+      <Form.Control type="file" className="w-auto" onChange={(event) => setSelectedFile(event.target.files[0])} />
+      {selectedFile && <Button onClick={onFileUpload}>Upload!</Button>}
+    </Stack>
+  </Form.Group >;
 }
 
 function FileListItem({ filename }) {
   const [isShowingConfirmation, setIsShowingConfirmation] = useState(false);
   const [isFileDeleted, setIsFileDeleted] = useState(false);
+  const target = useRef(null);
 
   function deleteFile() {
-    fetch(API_URL + "/files/delete/" + filename, { method: "DELETE" })
+    fetch(API_URL + '/files/delete/' + filename, { method: 'DELETE' })
       .catch((err) => console.error(err));
 
     setIsFileDeleted(true);
   }
 
-  return <p>
-    {filename}
-    {isFileDeleted ? <i>deleted</i> : <button onClick={() => setIsShowingConfirmation(true)}>Delete</button>}
-    {(isShowingConfirmation && !isFileDeleted) &&
-      (<>
-        <b>Delete?</b>
-        <button onClick={deleteFile}>yes</button> :
-        <button onClick={() => setIsShowingConfirmation(false)}>no</button>
-      </>)}
-  </p>;
+  return <Stack direction="horizontal" gap={3}>
+    <FileDownloadLink filename={filename} />
+    {isFileDeleted ?
+      <Badge bg="primary">Deleted</Badge> :
+      <Button ref={target} variant="primary" onClick={() => setIsShowingConfirmation(true)}>Delete</Button>}
+    <Overlay target={target.current} show={isShowingConfirmation && !isFileDeleted} placement="right">
+      {({
+        placement: _placement,
+        arrowProps: _arrowProps,
+        show: _show,
+        popper: _popper,
+        hasDoneInitialMeasure: _hasDoneInitialMeasure,
+        ...props
+      }) => (
+        <div
+          {...props}
+          style={{
+            position: 'absolute',
+            backgroundColor: 'rgba(255, 100, 100, 0.85)',
+            padding: '2px 10px',
+            color: 'white',
+            borderRadius: 3,
+            ...props.style,
+          }}
+        >
+          Delete?
+          <Button variant="secondary" onClick={deleteFile}>Yes</Button>
+          <Button variant="primary" onClick={() => setIsShowingConfirmation(false)}>No</Button>
+        </div>
+      )}
+    </Overlay>
+  </Stack>;
 }
 
 function FilesManagement() {
@@ -123,27 +186,30 @@ function FilesManagement() {
 
   return <>
     <FileUploadForm updateFilesListHandler={updateFilesList} />
-    <ul>
-      {filenames.map((filename) => <li key={filename}><FileListItem filename={filename} /></li>)}
-    </ul>
+    <hr/>
+    <h3>Uploaded files</h3>
+    <ListGroup>
+      {filenames.map((filename) => <ListGroup.Item key={filename}><FileListItem filename={filename} /></ListGroup.Item>)}
+    </ListGroup>
   </>;
 }
 
 
 
 function Header() {
-  return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/files">Files Management</Link></li>
-      </ul>
-    </nav>
-  );
+  return <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
+    <Navbar.Brand>Searcher</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/files">Files Management</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>;
 }
 
 function App() {
-
   return <>
     <BrowserRouter>
       <Header />
--- frontend/semantic-search-frontend/src/main.jsx
+++ frontend/semantic-search-frontend/src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 // import './index.css'
 import App from './App.jsx';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = document.getElementById("root");
 
 createRoot(root).render(
